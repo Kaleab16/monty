dc11f3d2d7c178bc2cddfde6e944cb9fb9d2a493
kaleab16 <classkaleb@gmail.com>
2026-06-14 05:19:38 -0700

    Implement pchar opcode

--- execute.c
+++ execute.c
@@ -59,6 +59,10 @@ else if (strcmp(opcode, "mod") == 0)
 {
 mod(stack, line_number);
 }
+else if (strcmp(opcode, "pchar") == 0)
+{
+pchar(stack, line_number);
+}
 else
 {
 fprintf(stderr, "L%u: unknown instruction %s\n", line_number, opcode);
--- monty.h
+++ monty.h
@@ -35,3 +35,5 @@ void _div(stack_t **stack, unsigned int line_number);
 void mul(stack_t **stack, unsigned int line_number);
 void sub(stack_t **stack, unsigned int line_number);
 void mod(stack_t **stack, unsigned int line_number);
+void pchar(stack_t **stack, unsigned int line_number);
+void pchar(stack_t **stack, unsigned int line_number);
